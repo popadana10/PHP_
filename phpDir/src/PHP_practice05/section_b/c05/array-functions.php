4f7f2c55a32fccc8297b08d6b25e3d7508668fae
Dana Popa <popa.dana100@gmail.com>
2024-03-26 11:01:59 +0200

    array functions

--- phpDir/src/PHP_practice05/section_b/c05/array-functions.php
+++ phpDir/src/PHP_practice05/section_b/c05/array-functions.php
@@ -1,10 +1,33 @@
 <?php
 // Write PHP code here
-declare(strict_types=1);
+// Create an array of greetings then get random values
+$greetings = ['Hi', 'Howdy'];
+$greetings_key = array_rand($greetings);
+$greetings = $greetings[$greetings_key];
+
+// Array of best sellers, count items, list top items
+$bestsellers = ['notebook', 'pencil', 'ink'];
+$bestsellers_count = count($bestsellers);
+$bestsellers_text = implode(',', $bestsellers);
+
+//Array holding customer details
+$customer = ['forename' => 'James',
+'surname'=> 'Bond', 'email' => 'jamesbond@gmail.com'];
+
+// If you have a customer firstname add it to greent
+if(array_key_exists('forename', $customer)) {
+  $greetings .= $customer['forename'];
+}
+
+
+
 ?>
 <?php include 'includes/header.php'; ?>
 
 <h1>Best Sellers</h1>
-//Write code here
+<p><?= $greeting ?></p>
+<p>Our top <?= $bestsellers_count ?> items today are:
+  <b><?= $bestsellers_text ?> </b>
+</p>
 
 <?php include 'includes/footer.php'; ?>
