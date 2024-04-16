4f89626a52f0cce121f35b38611286e73007c792
Dana Popa <popa.dana100@gmail.com>
2024-04-16 12:05:25 +0300

    update crypto()function practice

--- phpDir/src/PHP_practice01/8.php
+++ phpDir/src/PHP_practice01/8.php
@@ -1,44 +1,47 @@
-<?php include "functions.php"; ?>
-<?php include "includes/header.php";?>
+<!DOCTYPE html>
+<html lang="en">
 
-	<section class="content">
+<?php
+include "functions.php";
+include "includes/header.php";
+?>
 
-		<aside class="col-xs-4">
+<section class="content">
 
-		<?php Navigation();?>
-			
-			
-		</aside><!--SIDEBAR-->
-
-
-		
-	<article class="main-content col-xs-8">
-	
-	
-	<?php  
-/*  Step 1 -Make a variable with some text as value
-
-		Step 2 - Use crypt() function to encrypt it
-
-		Step 3 - Assign the crypt result to a variable
-
-		Step 4 - echo the variable
-
-	*/
-
-$password = "mypassword";
-$hasFormat = "$2y$05$";
-$salt = "asedfasefasdfasdfasdfasdfasdf22";
-$hashFormatandSalt = $hasFormat . $salt;
-$encryped_password = crypt($password, $hashFormatandSalt);
-echo $encryped_password;
-echo "<br>";
-
-	?>
+    <aside class="col-xs-4">
+        <?php Navigation(); ?>
+    </aside>
+    <!--SIDEBAR-->
 
 
+    <article class="main-content col-xs-8">
+
+    <?php
+    // Step 1: Make a variable with some text as a value
+    $text = "This is some text.";
+
+    // Step 2: Use crypt() function to encrypt it with a salt
+    // Generate a salt (you can use any method to generate a salt, here I'm using crypt)
+    $salt = '$2a$07$usesomadasdsadsadsadasdasdsaehtoa';
+    $encrypted_text = crypt($text, $salt);
+
+    // Step 3: Assign the crypt result to a variable
+    /*There's no need for an additional step to assign the result of crypt() to a variable because I already doing that in Step 2. */
+
+    // Step 4: Output the variable
+    echo "Original text: " . $text . "<br>";
+    echo "Encrypted text: " . $encrypted_text;
+    ?>
 
 
+    </article>
+    <!--MAIN CONTENT-->
 
-</article><!--MAIN CONTENT-->
-<?php include "includes/footer.php"; ?>
+</section>
+
+<?php
+include "includes/footer.php";
+?>
+
+</body>
+</html>
